5bd60cf22e355652349a25d23979ac601f0f9b9c
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-29 10:52:45 +0300

    init

---
+++ src/main/java/com/example/ProductStockReservation/ProductStockReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductStockReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStockReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStockReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProductStockReservation/ProductStockReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductStockReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStockReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
