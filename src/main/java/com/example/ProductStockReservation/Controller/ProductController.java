67ccc94461cb36ee17dd4f4737432d61f5e97b6d
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-29 15:27:15 +0300

    Lombok dependencies added

--- src/main/java/com/example/ProductStockReservation/Controller/ProductController.java
+++ src/main/java/com/example/ProductStockReservation/Controller/ProductController.java
@@ -1,8 +1,16 @@
 package com.example.ProductStockReservation.Controller;
 
+import com.example.ProductStockReservation.Entity.Product;
 import com.example.ProductStockReservation.Service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/products")
@@ -13,6 +21,35 @@ public class ProductController {
     public ProductController(ProductService productService){
         this.productService = productService;
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postProduct(@RequestBody @Valid Product product){
+
+        productService.save(product);
+
+    }
+    @GetMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Page<Product> postProduct(@RequestParam(defaultValue = "0") int page,
+                                     @RequestParam(defaultValue = "0") int size,
+                                     @RequestParam(defaultValue = "id") String sortBy,
+                                     @RequestParam(defaultValue = "0") boolean ascending){
+
+        Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page,size, sort);
+        return productService.findAllPageable(pageable);
+    }
+    @PostMapping("{id}/reserve")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product reserveProduct(@PathVariable UUID id,
+            @RequestParam int quantity){
+
+        return productService.reserveProduct();
+
+    }
+
+
 
 
 }
--- src/main/java/com/example/ProductStockReservation/Service/ProductService.java
+++ src/main/java/com/example/ProductStockReservation/Service/ProductService.java
@@ -1,6 +1,10 @@
 package com.example.ProductStockReservation.Service;
 
+import com.example.ProductStockReservation.Entity.Product;
 import com.example.ProductStockReservation.Repository.ProductRepositoryJPA;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,4 +16,13 @@ public class ProductService {
         this.productRepo = productRepo;
     }
 
+    public Product reserveProduct() {
+        return null;
+    }
+
+    public Page<Product> findAllPageable(Pageable pageable) {
+    }
+
+    public void save(@Valid Product product) {
+    }
 }
