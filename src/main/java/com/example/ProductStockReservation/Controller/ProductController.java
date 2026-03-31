40c08439eb530856e8337fd995db0211770e001b
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-31 14:51:24 +0300

    implemented the endpoints and functions

--- src/main/java/com/example/ProductStockReservation/Controller/ProductController.java
+++ src/main/java/com/example/ProductStockReservation/Controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.example.ProductStockReservation.Controller;
 
 import com.example.ProductStockReservation.Entity.Product;
+import com.example.ProductStockReservation.Exception.ErrorStructureException;
 import com.example.ProductStockReservation.Service.ProductService;
+import com.example.ProductStockReservation.dto.CreateProduct;
+import com.example.ProductStockReservation.dto.ProductReservation;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,8 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/products")
@@ -24,29 +25,29 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void postProduct(@RequestBody @Valid Product product){
+    public Product postProduct(@RequestBody @Valid CreateProduct createProduct){
 
-        productService.save(product);
+        Product product= new Product(null,createProduct.name(),createProduct.stock(),createProduct.version());
+
+        return productService.save(product).getOrElseThrow(ErrorStructureException::new);
 
     }
     @GetMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Page<Product> postProduct(@RequestParam(defaultValue = "0") int page,
-                                     @RequestParam(defaultValue = "0") int size,
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Product> getProducts(@RequestParam(defaultValue = "0") int page,
+                                     @RequestParam(defaultValue = "5") int size,
                                      @RequestParam(defaultValue = "id") String sortBy,
                                      @RequestParam(defaultValue = "0") boolean ascending){
 
         Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
         Pageable pageable = PageRequest.of(page,size, sort);
-        return productService.findAllPageable(pageable);
+
+        return productService.findAllPageable(pageable).getOrElseThrow(ErrorStructureException::new);
     }
-    @PostMapping("{id}/reserve")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Product reserveProduct(@PathVariable UUID id,
-            @RequestParam int quantity){
-
-        return productService.reserveProduct();
-
+    @PostMapping("/reserve")
+    @ResponseStatus(HttpStatus.OK)
+    public Product reserveProduct(@RequestBody @Valid ProductReservation productReservation){
+        return productService.reserveProduct(productReservation).getOrElseThrow(ErrorStructureException::new);
     }
 
 
--- src/main/java/com/example/ProductStockReservation/Service/ProductService.java
+++ src/main/java/com/example/ProductStockReservation/Service/ProductService.java
@@ -1,28 +1,112 @@
 package com.example.ProductStockReservation.Service;
 
 import com.example.ProductStockReservation.Entity.Product;
+import com.example.ProductStockReservation.Exception.ErrorType;
+import com.example.ProductStockReservation.Exception.StructuredError;
 import com.example.ProductStockReservation.Repository.ProductRepositoryJPA;
+import com.example.ProductStockReservation.dto.ProductReservation;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockingTaskExecutor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepositoryJPA productRepo;
+    private final LockingTaskExecutor executor;
 
-    public ProductService(ProductRepositoryJPA productRepo){
-        this.productRepo = productRepo;
+
+
+    public Either<StructuredError,Product> reserveProduct(ProductReservation requestedProduct) {
+
+        LockConfiguration lockConfig = new LockConfiguration(
+                Instant.now(),
+                "lock_product_" + requestedProduct.id(),
+                Duration.ofMinutes(1),
+                Duration.ZERO
+        );
+
+
+        return Try.of(() ->
+                        executor.executeWithLock(() -> {
+                            Option<Product> productOption = productRepo.findOptionById(requestedProduct.id());
+
+                            return productOption
+                                    .toEither(new StructuredError(
+                                            "Could Not Find Product",
+                                            ErrorType.NOT_FOUND_ERROR
+                                    ))
+                                    .flatMap(product -> {
+                                        if (product.getStock() < requestedProduct.quantity()) {
+                                            return Either.left(new StructuredError(
+                                                    "INSUFFICIENT_STOCK",
+                                                    ErrorType.SERVER_ERROR
+                                            ));
+                                        }
+
+                                        Try.run(() -> Thread.sleep(10000))
+                                                .onFailure(ex -> Thread.currentThread().interrupt())
+                                                .get();
+
+                                        product.setStock(product.getStock() - requestedProduct.quantity());
+                                        return Either.right(productRepo.save(product));
+                                    });
+                        }, lockConfig)
+                )
+                .toEither()
+                .mapLeft(ex -> new StructuredError(
+                        "Internal Server Error Reserving Product",
+                        ErrorType.SERVER_ERROR
+                ))
+                .flatMap(taskResult -> {
+                    if (!taskResult.wasExecuted()) {
+                        return Either.left(new StructuredError(
+                                "LOCK_NOT_ACQUIRED",
+                                ErrorType.SERVER_ERROR
+                        ));
+                    }
+
+                    Either<StructuredError, Product> result = taskResult.getResult();
+
+                    if (result == null) {
+                        return Either.left(new StructuredError(
+                                "LOCK_EXECUTED_BUT_RESULT_NULL",
+                                ErrorType.SERVER_ERROR
+                        ));
+                    }
+
+                    return result;
+                });
+
     }
 
-    public Product reserveProduct() {
-        return null;
+
+
+
+
+
+
+    public Either<StructuredError, Page<Product>> findAllPageable(Pageable pageable) {
+        return Try.of(() -> productRepo.findAll(pageable)).toEither().mapLeft(throwable -> new StructuredError("Error Loading Data",ErrorType.SERVER_ERROR));
     }
 
-    public Page<Product> findAllPageable(Pageable pageable) {
-    }
+    public Either<StructuredError, Product> save(@Valid Product product) {
 
-    public void save(@Valid Product product) {
+        return Try.of(() -> productRepo.save(product)).toEither().mapLeft(throwable -> new StructuredError("Issue when Saving The Product", ErrorType.SERVER_ERROR));
+
+
     }
 }
