c64d5946c44b2a7321eee25bba42871bb962b3a8
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-31 14:52:12 +0300

    locked save CRUD Operation added findOptionById

--- src/main/java/com/example/ProductStockReservation/Repository/ProductRepositoryJPA.java
+++ src/main/java/com/example/ProductStockReservation/Repository/ProductRepositoryJPA.java
@@ -1,6 +1,8 @@
 package com.example.ProductStockReservation.Repository;
 
 import com.example.ProductStockReservation.Entity.Product;
+import io.vavr.control.Option;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,12 @@ import java.util.UUID;
 
 @Repository
 public interface ProductRepositoryJPA extends JpaRepository<Product, UUID> {
+
+    @Override
+    @SchedulerLock(name = "product_save_lock", lockAtMostFor = "5m", lockAtLeastFor = "1m")
+    <S extends Product> S save(S product);
+
+    Option<Product> findOptionById(UUID id);
+
+
 }
