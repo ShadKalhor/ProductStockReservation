901a70930db3bf3832a29fa7e3c4abdc04defe64
shadKalhor <shad.jalal.aziz@gmail.com>
2026-04-02 12:44:43 +0300

    made ErrorStructureException.java send StructuredError Message to super.

--- src/main/java/com/example/ProductStockReservation/Exception/ErrorStructureException.java
+++ src/main/java/com/example/ProductStockReservation/Exception/ErrorStructureException.java
@@ -1,17 +1,21 @@
 package com.example.ProductStockReservation.Exception;
 
 
+import lombok.Getter;
+
 import static com.example.ProductStockReservation.Exception.ErrorTypeToHttpStatusMapper.httpStatus;
 
 
 public class ErrorStructureException extends RuntimeException {
 
+    @Getter
     private final int httpStatus;
 
+    @Getter
     private final String message;
 
     public ErrorStructureException(StructuredError structuredError){
-
+        super(structuredError.message());
         this.httpStatus = httpStatus(structuredError.type());
         this.message =structuredError.message();
 
