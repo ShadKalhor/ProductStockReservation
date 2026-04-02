652f6e790b1c69a1d5b6319b48631b5201059b36
shadKalhor <shad.jalal.aziz@gmail.com>
2026-04-02 11:44:12 +0300

    Added Logging in The Endpoints.

--- src/main/java/com/example/ProductStockReservation/Controller/ProductController.java
+++ src/main/java/com/example/ProductStockReservation/Controller/ProductController.java
@@ -2,10 +2,13 @@ package com.example.ProductStockReservation.Controller;
 
 import com.example.ProductStockReservation.Entity.Product;
 import com.example.ProductStockReservation.Exception.ErrorStructureException;
+import com.example.ProductStockReservation.Exception.ErrorType;
+import com.example.ProductStockReservation.Exception.StructuredError;
 import com.example.ProductStockReservation.Service.ProductService;
 import com.example.ProductStockReservation.dto.CreateProduct;
 import com.example.ProductStockReservation.dto.ProductReservation;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
@@ -26,6 +30,12 @@ public class ProductController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product postProduct(@RequestBody @Valid CreateProduct createProduct){
+        log.debug("Create Product Request Received. ProductName: {}, ProductStock: {}, ProductVersion: {}",
+                createProduct.name(), createProduct.stock(), createProduct.version());
+
+
+        log.debug("Mapping CreateProduct DTO to Product Entity. ProductName: {}, ProductStock: {}, ProductVersion: {}",
+                createProduct.name(), createProduct.stock(), createProduct.version() );
 
         Product product= new Product(null,createProduct.name(),createProduct.stock(),createProduct.version());
 
@@ -37,16 +47,35 @@ public class ProductController {
     public Page<Product> getProducts(@RequestParam(defaultValue = "0") int page,
                                      @RequestParam(defaultValue = "5") int size,
                                      @RequestParam(defaultValue = "id") String sortBy,
-                                     @RequestParam(defaultValue = "0") boolean ascending){
+                                     @RequestParam(defaultValue = "true") boolean ascending){
+
+        log.debug("Get Product Request Received.");
+
+
+        log.debug("Preparing Sort Configuration. Page: {}, Size: {}, SortBy: {}, IsAscending: {}",
+                page,
+                size,
+                sortBy,
+                ascending);
+
+        if (!sortBy.equals("id") && !sortBy.equals("name") && !sortBy.equals("stock")&& !sortBy.equals("version")) {
+
+            log.error("Invalid Sort Field. SortBy: {}", sortBy);
+            throw new ErrorStructureException(new StructuredError("Invalid Sort Field.", ErrorType.VALIDATION_ERROR));
+        }
 
         Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
         Pageable pageable = PageRequest.of(page,size, sort);
 
         return productService.findAllPageable(pageable).getOrElseThrow(ErrorStructureException::new);
     }
-    @PostMapping("/reserve")
+    @PostMapping("/reservations")
     @ResponseStatus(HttpStatus.OK)
     public Product reserveProduct(@RequestBody @Valid ProductReservation productReservation){
+
+        log.debug("Reserve Product Request Received. ProductId: {}, ReservingQuantity: {}",
+                productReservation.id(), productReservation.quantity());
+
         return productService.reserveProduct(productReservation).getOrElseThrow(ErrorStructureException::new);
     }
 
